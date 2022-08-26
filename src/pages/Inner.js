0453e902679a744d4b2619cad0fb28c2c91bef24
Jaba Beroshvili <jb.beroshvili@gmail.com>
2022-08-26 21:05:15 +0400

    created innerPage and About me section

---
+++ src/pages/Inner.js
@@ -0,0 +1,12 @@
+import Box from '../components/Box';
+import Button from '../components/Button'
+import '../assets/styles/innerPage.css'
+import '../index.css'
+function Inner() {
+    const text = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque';
+    return(
+        <Box title='About me' content={text} h2className='inner-page__title' pclassName='inner-page__p'></Box>
+    )
+}
+
+export default Inner;
